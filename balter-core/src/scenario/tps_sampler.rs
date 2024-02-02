eb10522d25629dcc2316667efbc94f62d750992e
Byron Wasti <byron.wasti@gmail.com>
2024-02-02 16:47:13 -0500

    Fix TPS sampling method
    The TPS sampling method used previously is garbage. It relied on each
    task completion to act as a timer for collecting the global TPS from the
    atomics, which meant that if two tasks completed right next to each
    other the second task would have next to no data (and therefore be quite
    inaccurate). This lead to instability in all of the various controllers.
    
    The new TPS sampling method is dead simple: each task contains a batch
    of the scenarios to be run (which is configured based on the number of
    transactions in a scenario). Each task completion gives a fraction of
    the total TPS (1/N where N is the number of tasks), which we can use to
    give an approximation of the total TPS. This is a far more accurate and
    robust method of measuring TPS.

--- balter-core/src/scenario/tps_sampler.rs
+++ balter-core/src/scenario/tps_sampler.rs
@@ -12,7 +12,7 @@ use std::{
     time::{Duration, Instant},
 };
 use tokio::task::JoinSet;
-use tracing::{debug, trace, warn};
+use tracing::{debug, error, trace};
 
 // TODO: Experimentally find values rather than just guessing.
 const DEFAULT_SAMPLE_SIZE: u64 = 20;
@@ -20,12 +20,9 @@ const DEFAULT_SAMPLE_SIZE: u64 = 20;
 pub(crate) struct TpsSampler {
     scenario: fn() -> BoxedFut,
     pub(crate) limiter: Option<Arc<DefaultDirectRateLimiter>>,
-    success: Arc<AtomicU64>,
-    error: Arc<AtomicU64>,
     pub(crate) concurrent_count: usize,
     pub(crate) batch_size: usize,
-    join_set: JoinSet<()>,
-    timer: Instant,
+    join_set: JoinSet<TpsData>,
     tps_limit: f64,
 }
 
@@ -35,59 +32,48 @@ impl TpsSampler {
             scenario,
             limiter: None,
             tps_limit: 0.,
-            success: Arc::new(AtomicU64::new(0)),
-            error: Arc::new(AtomicU64::new(0)),
             concurrent_count: 1,
             batch_size: 1,
             join_set: JoinSet::new(),
-            timer: Instant::now(),
         };
         new_self.set_tps_limit(goal_tps);
+        new_self.populate_jobs();
         new_self
     }
 
     pub(crate) async fn sample_tps(&mut self) -> Option<TpsData> {
-        let mut data = SampleData::new();
-        let mut res = None;
+        #[allow(clippy::redundant_pattern_matching)]
+        let res = if let Some(tps_data) = self.join_set.join_next().await {
+            // TODO: Properly handle errors
+            let tps_data = tps_data.unwrap();
 
-        while res.is_none() {
-            #[allow(clippy::redundant_pattern_matching)]
-            if let Some(_) = self.join_set.join_next().await {
-                // 1. On each finished scenario batch we check measurements.
-                let sample_data = self.sample_data();
-
-                // 2. Adjust our batch-size; ideally every time we return a batch of scenarios we
-                //    have enough data to make adjustments.
-                if sample_data.total() == 0 {
-                    // NOTE: Sometimes a scenario will finish without having any data; this is
-                    // because the scenario is bad (doesn't call any transactions) or because two
-                    // scenarios finish at nearly the same time.
-                    warn!("No transaction data for completed scenario.");
-                } else if sample_data.total() < DEFAULT_SAMPLE_SIZE {
-                    // TODO: Really simplistic adaptive batch controls here for scenarios. Would be
-                    // good to have something smarter, but this works and doesn't blow up.
-                    self.batch_size += 1;
-                    trace!("Increasing sampling batch size to {}", self.batch_size);
-                } else if sample_data.total() > 2 * DEFAULT_SAMPLE_SIZE {
-                    self.batch_size -= 1;
-                    trace!("Reducing sampling batch size to {}", self.batch_size);
-                }
-
-                // 3. Increment our sample counts.
-                data += sample_data;
-
-                // 4. Once we have enough data, we can make our timing measurement.
-                if data.total() >= DEFAULT_SAMPLE_SIZE {
-                    let elapsed = self.timer.elapsed();
-                    self.timer = Instant::now();
-
-                    res = Some(data.tps_data(elapsed));
-                }
+            if tps_data.total() == 0 {
+                error!("No transaction data for completed scenario.");
+                return None;
+            } else if tps_data.total() < DEFAULT_SAMPLE_SIZE {
+                // TODO: Really simplistic adaptive batch controls here for scenarios. Would be
+                // good to have something smarter, but this works and doesn't blow up.
+                self.batch_size += 1;
+                trace!("Increasing sampling batch size to {}", self.batch_size);
+            } else if tps_data.total() > 2 * DEFAULT_SAMPLE_SIZE {
+                self.batch_size -= 1;
+                trace!("Reducing sampling batch size to {}", self.batch_size);
             }
 
-            // Repopulate scenario runs
-            self.populate_jobs();
-        }
+            // TODO: Sharsty stats. Since each returned value is 1/N of the number of tasks, we
+            // just multiply here. But it means our success and error counts in the TpsData are
+            // fudged.
+            let mut tps_data = tps_data;
+            tps_data.success_count *= self.concurrent_count as u64;
+            tps_data.error_count *= self.concurrent_count as u64;
+
+            Some(tps_data)
+        } else {
+            error!("Something went wrong");
+            return None;
+        };
+
+        self.populate_jobs();
 
         res
     }
@@ -95,29 +81,31 @@ impl TpsSampler {
     fn populate_jobs(&mut self) {
         while self.join_set.len() < self.concurrent_count {
             let scenario = self.scenario;
+
+            let success = Arc::new(AtomicU64::new(0));
+            let error = Arc::new(AtomicU64::new(0));
             let transaction_data = TransactionData {
                 limiter: self.limiter.clone(),
-                success: self.success.clone(),
-                error: self.error.clone(),
+                success: success.clone(),
+                error: error.clone(),
             };
             let batch_size = self.batch_size;
             self.join_set
                 .spawn(TRANSACTION_HOOK.scope(transaction_data, async move {
-                    let _timer = Instant::now();
+                    let timer = Instant::now();
                     for _ in 0..batch_size {
                         scenario().await
                     }
+
+                    let elapsed = timer.elapsed();
+                    let success_count = success.fetch_min(0, Ordering::Relaxed);
+                    let error_count = error.fetch_min(0, Ordering::Relaxed);
+                    TpsData {
+                        success_count,
+                        error_count,
+                        elapsed,
+                    }
                 }));
-        }
-    }
-
-    fn sample_data(&self) -> SampleData {
-        let success_count = self.success.fetch_min(0, Ordering::Relaxed);
-        let error_count = self.error.fetch_min(0, Ordering::Relaxed);
-
-        SampleData {
-            success_count,
-            error_count,
         }
     }
 
@@ -147,56 +135,16 @@ pub(crate) struct TpsData {
 }
 
 impl TpsData {
-    pub(crate) fn tps(&self) -> f64 {
-        (self.success_count + self.error_count) as f64 / self.elapsed.as_nanos() as f64 * 1e9
+    pub fn tps(&self) -> f64 {
+        self.total() as f64 / self.elapsed.as_nanos() as f64 * 1e9
     }
 
-    pub(crate) fn error_rate(&self) -> f64 {
-        self.error_count as f64 / (self.success_count + self.error_count) as f64
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-struct SampleData {
-    success_count: u64,
-    error_count: u64,
-}
-
-impl SampleData {
-    fn new() -> Self {
-        Self {
-            success_count: 0,
-            error_count: 0,
-        }
+    pub fn error_rate(&self) -> f64 {
+        self.error_count as f64 / self.total() as f64
     }
 
-    fn total(&self) -> u64 {
+    pub fn total(&self) -> u64 {
         self.success_count + self.error_count
-    }
-
-    fn tps_data(&self, elapsed: Duration) -> TpsData {
-        TpsData {
-            success_count: self.success_count,
-            error_count: self.error_count,
-            elapsed,
-        }
-    }
-}
-
-impl std::ops::Add for SampleData {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            success_count: self.success_count + rhs.success_count,
-            error_count: self.error_count + rhs.error_count,
-        }
-    }
-}
-
-impl std::ops::AddAssign for SampleData {
-    fn add_assign(&mut self, other: Self) {
-        *self = *self + other;
     }
 }
 
@@ -227,6 +175,6 @@ mod tests {
         let _tps_data = tps_sampler.sample_tps().await;
         let prev_size = tps_sampler.batch_size;
         let _tps_data = tps_sampler.sample_tps().await;
-        assert_eq!(tps_sampler.batch_size, prev_size);
+        assert!(tps_sampler.batch_size > prev_size);
     }
 }
